ad9408a7cbbcd641372a8ea13355f49325fe0651
Peter Njiru <peternjirum@gmail.com>
2023-09-07 00:54:57 -0400

    completes chatterbox lab

--- server/app.py
+++ server/app.py
@@ -10,17 +10,91 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 CORS(app)
+
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+            
+        messages = []
+        for message in Message.query.order_by(Message.created_at.asc()).all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+
+        response = make_response(
+            messages,
+            200
+        )
+
+        return response
+    
+    elif request.method == 'POST':
+
+        request_data = request.json
+
+        new_message = Message(
+            body=request_data['body'],
+            username=request_data['username'],
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_dict = new_message.to_dict()
+
+        response = make_response(
+            jsonify(new_message_dict),
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods=['PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    if request.method == 'PATCH':
+            message = Message.query.filter(Message.id == id).first()
+
+            # for attr in request.form:
+            #     setattr(message, attr, request.form.get(attr))
+
+            # Extracts JSON data from the request body
+            request_data = request.json
+
+            message.body=request_data['body']
+
+            db.session.add(message)
+            db.session.commit()
+
+            message_dict = message.to_dict()
+
+            response = make_response(
+                jsonify(message_dict),
+                200
+            )
+
+            return response
+     
+    elif request.method == 'DELETE':
+            message = Message.query.filter(Message.id == id).first()
+
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+            "delete_successful": True,
+            "message": "Message deleted."    
+            }
+
+            response = make_response(
+             response_body,
+             200
+            )
+
+            return response
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=4000)
